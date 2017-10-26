0d538df7eb661734da9025f295a110fa01224b9b
codecloude <pkcemk.5@gmail.com>
2017-10-27 04:37:23 +0530

    third commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 import {CommonService} from './common.service'; 
+import {trigger,state,style,transition,animate,keyframes} from '@angular/animations'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations:[
+    trigger('myAwesomeAnimations',[
+      state('small',style({
+        transform: 'scale(1)'
+      })),
+      state('large',style({
+        transform: 'scale(1.2)'
+      })),
+      transition('small <=> large', animate('300ms ease-in',style({
+        transform: 'translateY(40px)'
+      })))
+    ]),
+  ]
 })
 export class AppComponent {
 
@@ -31,9 +45,11 @@ export class AppComponent {
   buttonStatusString = "disableNow";
 
   clickCount = 0;
+  state:string = 'small';
   clickEvent(event){
     this.clickCount++;
     console.log(event);
+    this.state = (this.state === 'small'?'large':'small');
   }
   mouseEnter(event){
     this.clickCount--;
